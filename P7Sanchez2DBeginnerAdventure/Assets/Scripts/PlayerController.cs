141ef5593ac4010885b2851bec273e13f88bcc14
752071-tech <752071@student.dusd.net>
2025-11-18 15:27:57 -0800

    updated map and movement
    I changed my map and made it bigger. I also made it to where i control my character.

--- P7Sanchez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7Sanchez2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
+    public InputAction LeftAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeftAction.Enable();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -0.3f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 0.3f;
+        }
+        Debug.Log(horizontal);
+
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 0.3f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -0.3f;
+        }
+        Debug.Log(vertical);
+
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
+
 }
